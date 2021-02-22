ac9346d4b1c048705d11024b4e6dd035a0200b06
kyle-stuart <strtkyl@gmail.com>
2021-02-22 21:15:44 +0000

    Added current time variable.
    Trimmed some unneeded lines

--- weather_scraper_v1.py
+++ weather_scraper_v1.py
@@ -2,6 +2,13 @@ import json
 import xmltodict
 import requests
 
+from datetime import datetime, timedelta
+
+#set current time, rounded up to the hour
+current_time = datetime.now() + timedelta(hours=1)
+current_time = current_time.strftime("%H:%M:%S")
+current_time = current_time[0:2] + ":00:00"
+
 def weather_retrieval(latitude, longitude):
 
     URL = "http://metwdb-openaccess.ichec.ie/metno-wdb2ts/locationforecast?lat=" + str(latitude) + ";long=" + str(longitude)
@@ -15,14 +22,14 @@ def weather_retrieval(latitude, longitude):
     weather_obj = json.loads(json_weather)
     weather_dic = weather_obj['weatherdata']
 
-    time, temp_val, cloudi_val, wind_val, rain_val, clock_time, date = '', '', '', '', '', '', ''
+    time, temp_val, cloudi_val, wind_val, rain_val, clock_time, date, humidity_val = '', '', '', '', '', '', '', ''
 
+    #Loop through XML
     for i in range(0, 48, 2):
         for obj in weather_dic['product']['time'][i]:
             data_titles_overview = weather_dic['product']['time'][i]
             for data in data_titles_overview:
                 weather_status = data_titles_overview[data]
-                weather_string_and_dic = weather_status
                 for key in weather_status:
                     if len(key) == 1 and key != " ":
                         time += key
@@ -38,18 +45,21 @@ def weather_retrieval(latitude, longitude):
                                 data = weather_each[key2]
                                 if column == 'temperature' and '@value' in key2:
                                     temp_val = data + ' '
-                                if column == 'humidity' and '@value' in key2:
+                                elif column == 'humidity' and '@value' in key2:
                                     humidity_val = data
-                                if column == 'cloudiness' and '@percent' in key2:
+                                elif column == 'cloudiness' and '@percent' in key2:
                                     cloudi_val = data
-                                if column == 'windSpeed' and '@mps' in key2:
+                                elif column == 'windSpeed' and '@mps' in key2:
                                     wind_val = data
-                                if column == 'precipitation' and '@value' in key2:
+                                elif column == 'precipitation' and '@value' in key2:
                                     rain_val = data
             if temp_val == '':
                 pass
             else:
-                print(date + ' ' + clock_time + ' ' + temp_val + ' ' + wind_val + ' ' + cloudi_val + ' ' + rain_val)
+                if clock_time == current_time:
+                    # If weather data is the closest forecast
+                    print("PLACEHOLDER: WRITE THIS DATA TO WEATHER HISTORY DB")
+                print(date + ' ' + clock_time + ' ' + temp_val + ' ' + wind_val + ' ' + cloudi_val + ' ' + rain_val + ' ' + humidity_val)
                 temp_val = ''
                 wind_val = ''
                 cloudi_val = ''
@@ -57,7 +67,7 @@ def weather_retrieval(latitude, longitude):
                 clock_time = ''
                 date = ''
                 time = ''
+                humidity_val = ''
                 break
 
-
-weather_retrieval(54.7211, -8.7237)
+weather_retrieval(53.2734, -7.77832031)
