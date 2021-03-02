f27a933ab041235ae6a73334d324b0a32113f173
Michelle Duggan <michelle.duggan@ucdconnect.ie>
2021-03-02 19:16:38 +0000

    flask map, app file, templates and static files

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,1 +1,27 @@
-#Placeholder for flask application
+from flask import Flask, render_template
+import pandas as pd
+from jinja2 import Template
+from sqlalchemy import create_engine
+import dbinfo
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/")
+
+@app.route("/index")
+def hello():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return app.send_static_file("about.html")
+
+
+@app.route("/stations")
+def stations():
+    engine = create_engine(f"mysql+mysqlconnector://{dbinfo.user}:{dbinfo.passwd}@{dbinfo.host}:3306/{dbinfo.database}")
+   #result = engine.execute("select * from weather_hourDB.dbbikes_current_info")
+    df = pd.read_sql_table("dbbikes_current_info", engine)
+    print(df.head(3).to_json(orient="records"))
+    return df.to_json(orient="records")
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
