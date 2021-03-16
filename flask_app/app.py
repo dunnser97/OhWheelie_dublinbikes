af8d6f5369f508061cd3629a5f1fbfba2c6825b6
Shane <dunnser97@gmail.com>
2021-03-16 02:42:58 +0000

    -Consolidated changes to app.py between two conflicting files. -Issue with line 46 on loading pages on localhost --Ask others to confirm contains all important details

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,7 +3,14 @@ import pandas as pd
 from jinja2 import Template
 from sqlalchemy import create_engine
 import dbinfo
+from flask_caching import Cache
 app = Flask(__name__, template_folder='templates')
+
+cache = Cache()
+app = Flask(__name__, template_folder='templates')
+app.config['CACHE_TYPE'] = 'simple'
+
+cache.init_app(app)
 
 @app.route("/")
 
@@ -32,6 +39,7 @@ def about():
 
 
 @app.route("/stations")
+@cache.cached(timeout=60)
 def stations():
     engine = create_engine(f"mysql+mysqlconnector://{dbinfo.user}:{dbinfo.passwd}@{dbinfo.host}:3306/{dbinfo.database}")
    #result = engine.execute("select * from weather_hourDB.dbbikes_current_info")
@@ -51,19 +59,9 @@ def station(station_id):
     results = y.to_json(orient="split")
     return render_template("stations_individual.html", indiv_stat=x, weather_data=y, results=results)
 
-@app.route("/index/<int:station_id>")
-def weather(station_id):
-    engine = create_engine(f"mysql+mysqlconnector://{dbinfo.user}:{dbinfo.passwd}@{dbinfo.host}:3306/{dbinfo.database}",
-                           echo=True)
-    row_query = "select * from dbbikes_current_info where Station_number = " + str(format(station_id))
-    x = pd.read_sql_query(row_query, engine)
-    lat = x["latitude"]
-    long = x["longitude"]
-    row_query_1 = "select * from weather_forecast where latitude = " + str(format(lat[0])) + "and longitude = " + str(
-        format(long[0])) + "group by clock_time"
-    y = pd.read_sql_query(row_query_1, engine)
-    return y.to_json(orient="records")
+@app.route("/allstations")
+def allstations():
+    return render_template("allstations.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
-
