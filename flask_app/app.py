5718a0bcc47b7daf3ed63d4c7e3b7398c50663c6
Shane <dunnser97@gmail.com>
2021-03-23 10:17:45 +0000

    -Addition of mean available bikes by day and mean available bikes by time - Still requires updates to time so only checks for that day of week

--- flask_app/app.py
+++ flask_app/app.py
@@ -48,12 +48,17 @@ def stations():
 @cache.cached(timeout=600)
 def station(station_id):
     engine = create_engine(f"mysql+mysqlconnector://{dbinfo.user}:{dbinfo.passwd}@{dbinfo.host}:3306/{dbinfo.database}", echo=True)
+    sd_engine = create_engine(f"mysql+mysqlconnector://{dbinfo.dbuser}:{dbinfo.dbpasswd}@{dbinfo.dbhost}:3306/{dbinfo.dbdatabase}", echo=True)
     row_query = "select * from dbbikes_current_info where Station_number = " + str(format(station_id))
     x = pd.read_sql_query(row_query, engine)
     lat = x["latitude"]
     long = x["longitude"]
     row_query_1 = "select * from weather_forecast where latitude = " + str(format(lat[0])) + "and longitude = " + str(format(long[0])) + "group by clock_time"
     y = pd.read_sql_query(row_query_1, engine)
+    mean_day = "select AVG(available_bike_stands), weekday(date) from dbbikes_info where Station_number = " + str(format(station_id)) + " and time Between '06:00:00' AND '20:00:00' group by weekday(date)"
+    mean_day = pd.read_sql_query(mean_day, sd_engine)
+    mean_time ="select AVG(available_bike_stands), hour(time) from dbbikes_info where Station_number = " + str(format(station_id)) + " group by hour(time) order by hour(time) asc"
+    mean_time = pd.read_sql_query(mean_time, sd_engine)
     results = y.to_json(orient="split")
     return render_template("stations_individual.html", indiv_stat=x, weather_data=y, results=results)
 
