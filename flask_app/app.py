2ddf946fecc32093c39e207a330ad7fc21cfabb8
Shane <dunnser97@gmail.com>
2021-03-25 14:10:01 +0000

    -removed results and echo true as redundant for the minute

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sqlalchemy import create_engine
 import dbinfo
 from flask_caching import Cache
+import time
 
 cache = Cache()
 app = Flask(__name__, template_folder='templates')
@@ -47,8 +48,8 @@ def stations():
 @app.route("/allstations/<int:station_id>")
 @cache.cached(timeout=600)
 def station(station_id):
-    engine = create_engine(f"mysql+mysqlconnector://{dbinfo.user}:{dbinfo.passwd}@{dbinfo.host}:3306/{dbinfo.database}", echo=True)
-    sd_engine = create_engine(f"mysql+mysqlconnector://{dbinfo.dbuser}:{dbinfo.dbpasswd}@{dbinfo.dbhost}:3306/{dbinfo.dbdatabase}", echo=True)
+    engine = create_engine(f"mysql+mysqlconnector://{dbinfo.user}:{dbinfo.passwd}@{dbinfo.host}:3306/{dbinfo.database}")
+    sd_engine = create_engine(f"mysql+mysqlconnector://{dbinfo.dbuser}:{dbinfo.dbpasswd}@{dbinfo.dbhost}:3306/{dbinfo.dbdatabase}")
     row_query = "select * from dbbikes_current_info where Station_number = " + str(format(station_id))
     x = pd.read_sql_query(row_query, engine)
     lat = x["latitude"]
@@ -59,8 +60,7 @@ def station(station_id):
     mean_day = pd.read_sql_query(mean_day, sd_engine)
     mean_time ="select AVG(available_bike_stands), hour(time) from dbbikes_info where Station_number = " + str(format(station_id)) + " group by hour(time) order by hour(time) asc"
     mean_time = pd.read_sql_query(mean_time, sd_engine)
-    results = y.to_json(orient="split")
-    return render_template("stations_individual.html", indiv_stat=x, weather_data=y, results=results)
+    return render_template("stations_individual.html", indiv_stat=x, weather_data=y)
 
 @app.route("/allstations")
 def allstations():
