1726db8aeecc282055a0a8798953df6885492e53
Shane <dunnser97@gmail.com>
2021-04-11 22:52:01 +0100

    - Commenting and clear defunct functions

--- flask_app/static/individ.js
+++ flask_app/static/individ.js
@@ -69,28 +69,29 @@ function Temperature() {
          })
         }
 function draw_avg_bikes() {
-            localStorage.setItem('bike_function', 'time');
-
-            fetch("/index/"+ x + "/chart").then(response => {
-                return response.json(); }).then(data3 => {
-                    var array = [];
-                    var Header= ['Time', 'Avg Bikes'];
-                    array.push(Header);
-                    data3.forEach(hour => {
-                        var temp=[];
-                        temp.push(hour.T, parseFloat(hour.avg));
-                        array.push(temp);
-                    })
-                    var options = {
-                        title: 'Average Bikes per hour for Station ' + x,
-                        color: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-                        vAxis: {title: 'Bikes Available',  titleTextStyle: {color: '#333'},
-                        ticks: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32]}
-                    };
-                var chart = google.visualization.arrayToDataTable(array)
-                var chart_div = new google.visualization.ColumnChart(document.getElementById("bike_values"));
-                chart_div.draw(chart, options)
-            }).catch(err => {
+    //Loads average number of bikes per hour graph on individual pages
+    localStorage.setItem('bike_function', 'time');
+    fetch("/index/"+ x + "/chart").then(response => {
+        return response.json(); }).then(data3 => {
+            var array = [];
+            var Header= ['Time', 'Avg Bikes'];
+            array.push(Header);
+            data3.forEach(hour => {
+                var temp=[];
+                temp.push(hour.T, parseFloat(hour.avg));
+                array.push(temp);
+            })
+            var options = {
+                title: 'Average Bikes per hour for Station ' + x,
+                color: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
+                vAxis: {title: 'Bikes Available',  titleTextStyle: {color: '#333'},
+                ticks: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32]}
+            };
+        var chart = google.visualization.arrayToDataTable(array)
+        //Calls the bike values id from the html page and appends the graph to that div
+        var chart_div = new google.visualization.ColumnChart(document.getElementById("bike_values"));
+        chart_div.draw(chart, options)
+    }).catch(err => {
         console.log(err);
         document.getElementById('bike_values').innerHTML = '<h3>The bike charts for'+
          ' this station cannot be loaded at this time</h3>';
@@ -99,78 +100,45 @@ function draw_avg_bikes() {
         }
 
 function avg_bikes_day() {
-            localStorage.setItem('bike_function', 'day');
-            fetch("/allstations/"+ x + "/avg_bikes_day").then(response => {
-                return response.json(); }).then(data4 => {
-                    var array = [];
-                    var Header= ['Time', 'Avg Bikes'];
-                    array.push(Header);
-                    data4.forEach(day => {
-                        var temp=[];
-                        var weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-                        temp.push(day.day, parseFloat(day.avg_day));
-                        array.push(temp);
-                    })
-                    var options = {
-                        title: 'Average Bikes per day for Station ' + x,
-                        color: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
-                        vAxis: {title: 'Bikes Available',  titleTextStyle: {color: '#333'},
-                        ticks: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32]}
-                    };
-                var chart = google.visualization.arrayToDataTable(array)
-                var chart_div = new google.visualization.ColumnChart(document.getElementById("bike_values"));
-                chart_div.draw(chart, options)
-            }).catch(err => {
-                console.log(err);
-                document.getElementById('bike_values').innerHTML = '<h3>The bike charts for'+
-         ' this station cannot be loaded at this time</h3>';
-         })
-            document.getElementById("bike_values").style.display = "block";
-        }
+    //Loads average number of bikes per day graph on individual pages
+    localStorage.setItem('bike_function', 'day');
+    fetch("/allstations/"+ x + "/avg_bikes_day").then(response => {
+        return response.json(); }).then(data4 => {
+            var array = [];
+            var Header= ['Time', 'Avg Bikes'];
+            array.push(Header);
+            data4.forEach(day => {
+                var temp=[];
+                var weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+                temp.push(day.day, parseFloat(day.avg_day));
+                array.push(temp);
+            })
+            var options = {
+                title: 'Average Bikes per day for Station ' + x,
+                color: ['#e0440e', '#e6693e', '#ec8f6e', '#f3b49f', '#f6c7b6'],
+                vAxis: {title: 'Bikes Available',  titleTextStyle: {color: '#333'},
+                ticks: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32]}
+            };
+        var chart = google.visualization.arrayToDataTable(array)
+        var chart_div = new google.visualization.ColumnChart(document.getElementById("bike_values"));
+        chart_div.draw(chart, options)
+    }).catch(err => {
+        console.log(err);
+        document.getElementById('bike_values').innerHTML = '<h3>The bike charts for'+
+     ' this station cannot be loaded at this time</h3>';
+        })
+    document.getElementById("bike_values").style.display = "block";
+    }
 function timechange()   {
+    //Assigns Time for select option loops over 24 hour period in format HH:MM:SS
         var today = new Date();
-        var h = today.getHours();
-        var m = today.getMinutes();
-        var test = document.getElementById("times");
-        if (m>30)  {
-            h+=1;
-            m = ":00";
+        var time_link = document.getElementById("times");
+        for (i=0; i<24; i++)    {
+            current_time = i.toString() + ":00:00"
+            time_link.innerHTML = time_link.innerHTML + '<option value="' + i + '">' + current_time + '</option>';
         }
-        else if (m < 31)   {
-            m = ":00";
-        }
-        for (i=h; i<24; i++)    {
-            current_time = i.toString() + m + ":00"
-            test.innerHTML = test.innerHTML + '<option value="' + i + '">' + current_time + '</option>';
-        }
-        for (i=0; i<h; i++)    {
-            current_time = i.toString() + m + ":00"
-            test.innerHTML = test.innerHTML + '<option value="' + i + '">' + current_time + '</option>';
-        }
-                console.log(h);
 }
-function daychange()   {
-        var today = new Date();
-        var h = today.getHours();
-        var m = today.getMinutes();
-        var test = document.getElementById("times");
-        if (m>30)  {
-            h+=1;
-            m = ":00";
-        }
-        else if (m < 31)   {
-            m = ":00";
-        }
-        for (i=h; i<24; i++)    {
-            current_time = i.toString() + m + ":00"
-            test.innerHTML = test.innerHTML + '<option value="' + i + '">' + current_time + '</option>';
-        }
-        for (i=5; i<h; i++)    {
-            current_time = i.toString() + m + ":00"
-            test.innerHTML = test.innerHTML + '<option value="' + i + '">' + current_time + '</option>';
-        }
-                console.log(h);
-}
+
 Temperature()
 draw_avg_bikes()
 timechange()
@@ -196,18 +164,3 @@ function resize_charts(){
     }
     else if (bike_chart == 'day')
     avg_bikes_day() }
-/*
-$(document).ready(function(){
-    $('.updateButton').on('click', function() {
-    document.forms[0].days.value;
-        var days = document.getElementbyID("days").value;
-        var time = document.getElementbyID("time").value;
-        console.log(days);
-        req = $.ajax({
-            url : '/allstations/<int:station_id>'
-            type : 'POST'
-            data : { days : days, time :time}
-            });
-    });
-});
-*/
