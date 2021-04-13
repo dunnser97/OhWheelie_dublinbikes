324b2312e1ee32736c80c0b28d6f81360a684cb4
kyle-stuart <70216693+kyle-stuart@users.noreply.github.com>
2021-04-13 16:10:29 +0100

    Update stations.js
    Adde comments.

--- flask_app/static/stations.js
+++ flask_app/static/stations.js
@@ -1,9 +1,14 @@
+/* Filter search function which will update the contents of the results table, as the user types. Implementation aided by WW3 schools' Javascript tutorials.*/
 function filterSearch() {
     var input, filter, table, th, a, i, txtValue;
     input = document.getElementById("userInput");
     table = document.getElementById("resultsTable");
+    
+    //Search won't be case sensitive.
     filter = input.value.toUpperCase();
     th = table.getElementsByClassName("stname");
+    
+    //Anything not matching user query is hidden.
     for (i = 0; i < th.length; i++) {
         a = th[i].getElementsByTagName("a")[0];
         txtValue = a.textContent || a.innerText;
@@ -18,15 +23,16 @@ function filterSearch() {
 fetch("/stations").then(response => {
     return response.json();
 }).then(data => {
-    //console.log("data: ", data);
 
     for (var key in data){
         stations = JSON.parse(data['stations'])
     }
     console.log(stations)
+    //Table begins.
     results_output = "<table id='resultsTable'>"
     results_output += "<tr> <th> Station Name </th> <th> Available Bikes </th> <th> Available Stands </th> </tr>"
-
+    
+    //Loop through all stations and add a table row for each station.
     stations.forEach(station => {
         results_output += "<tr class='stname'>";
         results_output += "<th class='stname'><a href='/allstations/" + station.Station_number + "'>" + station.name + "</a></th>";
@@ -34,6 +40,7 @@ fetch("/stations").then(response => {
         results_output += "<th>" + station.available_bike_stands + "</th>";
         results_output += "</tr>";
     })
+    //Table ends.
     results_output += "</table>";
     document.getElementById("searchresults").innerHTML = results_output;
 
